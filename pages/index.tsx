3aaef8f63deeed9b314783100ae26a2db76ec255
balogdrene <103446696+balogdrene@users.noreply.github.com>
2022-07-01 15:19:50 +0200

    first version

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,84 +1,154 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import type { NextPage } from "next"
+import Head from "next/head"
+
+import { useState } from "react"
 
 const Home: NextPage = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+  const [isFeaturesOpen, setIsFeaturesOpen] = useState(false)
+  const [isCompanyOpen, setIsCompanyOpen] = useState(false)
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="">
       <Head>
-        <title>Create Next App</title>
+        <title>Intro section with dropdown menu</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <header className="flex items-center justify-between p-5">
+        <div className="lg:flex lg:items-start lg:justify-start">
+          <img src="logo.svg" alt="" className="lg:mr-5" />
+          <nav className={`navbar ${isMenuOpen ? "open" : ""} `}>
+            <div>
+              <button
+                onClick={() => setIsFeaturesOpen((prevState) => !prevState)}
+                className="flex items-center justify-start gap-2 text-sm place-items-center"
+              >
+                Features{" "}
+                {!isFeaturesOpen ? (
+                  <img src="icon-arrow-down.svg" alt="" />
+                ) : (
+                  <img src="icon-arrow-up.svg" alt="" />
+                )}
+              </button>
+              <ul
+                className={`mt-2 ml-3 ${
+                  isFeaturesOpen ? "block" : "hidden"
+                } lg:ml-0 lg:p-3 lg:shadow`}
+              >
+                <li className="flex items-center justify-start gap-2 mb-2 text-sm place-items-center">
+                  <img src="icon-todo.svg" alt="" />
+                  Todo List
+                </li>
+                <li className="flex items-center justify-start gap-2 mb-2 text-sm place-items-center">
+                  <img src="icon-calendar.svg" alt="" />
+                  Calendar
+                </li>
+                <li className="flex items-center justify-start gap-2 mb-2 text-sm place-items-center">
+                  <img src="icon-reminders.svg" alt="" />
+                  Reminder
+                </li>
+                <li className="flex items-center justify-start gap-2 mb-2 text-sm place-items-center">
+                  <img src="icon-planning.svg" alt="" />
+                  Planning
+                </li>
+              </ul>
+            </div>
+            <div>
+              <button
+                onClick={() => setIsCompanyOpen((prevState) => !prevState)}
+                className="flex items-center justify-start gap-2 mb-2 text-sm place-items-center"
+              >
+                Company{" "}
+                {!isCompanyOpen ? (
+                  <img src="icon-arrow-down.svg" alt="" />
+                ) : (
+                  <img src="icon-arrow-up.svg" alt="" />
+                )}
+              </button>
+              <ul
+                className={`mt-2 ml-3 ${
+                  isCompanyOpen ? "block" : "hidden"
+                } lg:ml-0 lg:p-3 lg:shadow`}
+              >
+                <li className="flex items-center justify-start gap-2 mb-2 text-sm place-items-center">
+                  History
+                </li>
+                <li className="flex items-center justify-start gap-2 mb-2 text-sm place-items-center">
+                  Our Team
+                </li>
+                <li className="flex items-center justify-start gap-2 mb-2 text-sm place-items-center">
+                  Blog
+                </li>
+              </ul>
+            </div>
+            <div className="mb-2 lg:mb-0">
+              <button>Careers</button>
+            </div>
+            <div>
+              <button>About</button>
+            </div>
+          </nav>
         </div>
-      </main>
+        <div>
+          <button
+            className="lg:hidden"
+            onClick={() => setIsMenuOpen((prevState) => !prevState)}
+          >
+            {!isMenuOpen ? (
+              <img src="icon-menu.svg" alt="" />
+            ) : (
+              <img src="icon-close-menu.svg" alt="" />
+            )}
+          </button>
+        </div>
 
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
+        <div className="hidden lg:block">
+          <button className="mr-10 opacity-75">Login</button>
+          <button className="px-6 py-2 border-2 border-black opacity-75 rounded-2xl">
+            Register
+          </button>
+        </div>
+      </header>
+      <section className="flex flex-col-reverse max-w-4xl py-20 md:gap-10 md:grid md:grid-cols-2 md:place-items-center lg:max-w-5xl md:mx-auto">
+        <article className="px-5 mt-10 text-center md:text-left xl:px-0 md:mt-0">
+          <h1 className="mb-5 text-4xl font-bold lg:text-6xl xl:text-7xl">
+            Make remote work
+          </h1>
+          <p className="mb-5">
+            Get your team in sync, no matter your location. Streamline
+            processes, create team rituals, and watch productivity soar.
+          </p>
+          <button className="px-6 py-2 font-bold text-white transition-all duration-150 bg-black rounded-lg shadow hover:opacity-75">
+            Learn more
+          </button>
+          <ul className="flex items-center justify-center gap-5 mt-10 place-items-center md:place-items-start">
+            <li>
+              <img src="client-databiz.svg" alt="" className="w-16 md:w-24" />
+            </li>
+            <li>
+              <img
+                src="client-audiophile.svg"
+                alt=""
+                className="w-16 md:w-24"
+              />
+            </li>
+            <li>
+              <img src="client-meet.svg" alt="" className="w-16 md:w-24" />
+            </li>
+            <li>
+              <img src="client-maker.svg" alt="" className="w-16 md:w-24" />
+            </li>
+          </ul>
+        </article>
+        <article>
+          <picture>
+            <source
+              media="(min-width: 768px)"
+              srcSet="image-hero-desktop.png"
+            />
+            <img src="image-hero-mobile.png" alt="" className="w-full" />
+          </picture>
+        </article>
+      </section>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        almostWhite: "hsl(0, 0%, 98%)",
+        mediumGray: "hsl(0, 0%, 41%)",
+        almostBlack: "hsl(0, 0%, 8%)",
+      },
+      fontFamily: {
+        epilogue: "Epilogue",
+      },
+    },
   },
   plugins: [],
 }
